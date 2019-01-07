4594ca7e4d0ebddad821ad01597a6d156bf662ca
dististik <austin4444@gmail.com>
2019-01-06 16:36:16 -0800

    Repurpose for Monogatari songs

---
+++ src/js/data/Monogatari-Songs_2019-1.js
@@ -0,0 +1,264 @@
+dataSetVersion = "Monogatari-Songs_2019-1";
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Song Type",
+    key: "type",
+    tooltip: "Click this to filter by specific songs.",
+    checked: false,
+    sub: [
+      { name: "Opening Songs", key: "OPs" },
+      { name: "Ending Songs", key: "EDs" },
+      { name: "BD Songs", key: "Extras" }
+    ]
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "staple stable",
+    img: "V3LNDyB.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "ambivalent world",
+    img: "KcQCbfE.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "Kaerimichi",
+    img: "SaFxPzX.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "Renai Circulation",
+    img: "atPX4yl.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "sugar sweet nightmare",
+    img: "sW0rVf3.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "Kimi no Shiranai Monogatari",
+    img: "y1LA3Ma.jpg",
+    opts: {
+      type: ["EDs"]
+    }
+  },
+  {
+    name: "perfect slumbers",
+    img: "2OyaJlO.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "Kieru daydream",
+    img: "JPVHVM1.jpg",
+    opts: {
+      type: ["EDs"]
+    }
+  },
+  {
+    name: "Futakotome",
+    img: "qUc6UhS.png",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "marshmallow justice",
+    img: "XV6qUJW.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "Shirogane Disco",
+    img: "6P6JLlt.png",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "Naisho no Hanashi",
+    img: "HA4o24O.png",
+    opts: {
+      type: ["EDs"]
+    }
+  },
+  {
+    name: "chocolate insomnia",
+    img: "A6ZE74t.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "happy bite",
+    img: "6HWzwAU.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "white lies",
+    img: "pSGBZEH.jpg",
+    opts: {
+      type: ["Extras"]
+    }
+  },
+  {
+    name: "Mousou Express",
+    img: "kXiJx7N.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "fast love",
+    img: "Y8aoLcB.png",
+    opts: {
+      type: ["Extras"]
+    }
+  },
+  {
+    name: "Kogarashi Sentiment",
+    img: "OzuZ8kO.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "Ai wo Utae",
+    img: "dVPEK9C.jpg",
+    opts: {
+      type: ["EDs"]
+    }
+  },
+  {
+    name: "Sono Koe wo Oboeteru",
+    img: "5urY7AX.jpg",
+    opts: {
+      type: ["EDs"]
+    }
+  },
+  {
+    name: "the last day of my adolescence",
+    img: "a9MbT94.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "Hanaato -shirushi-",
+    img: "WAsJgjm.png",
+    opts: {
+      type: ["EDs"]
+    }
+  },
+  {
+    name: "orange mint",
+    img: "xVX0jKz.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "border",
+    img: "hE3GdKe.jpg",
+    opts: {
+      type: ["EDs"]
+    }
+  },
+  {
+    name: "decent black",
+    img: "kFiWH66.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "Yuudachi Houteishiki",
+    img: "ZNgCBZy.jpg",
+    opts: {
+      type: ["Extras"]
+    }
+  },
+  {
+    name: "mathemagics",
+    img: "t29Uubx.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "mein schatz",
+    img: "PzcJNYV.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "étoile et toi",
+    img: "ihb6A9R.jpg",
+    opts: {
+      type: ["EDs"]
+    }
+  },
+  {
+    name: "terminal terminal",
+    img: "sL57wwB.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "dark cherry mystery",
+    img: "k9F6t5P.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "dreamy date drive",
+    img: "syp41x4.jpg",
+    opts: {
+      type: ["OPs"]
+    }
+  },
+  {
+    name: "SHIORI",
+    img: "hMQaMaP.jpg",
+    opts: {
+      type: ["EDs"]
+    }
+  },
+  {
+    name: "Sayonara no Yukue",
+    img: "4VlYgIH.jpg",
+    opts: {
+      type: ["EDs"]
+    }
+  },
+  {
+    name: "whiz",
+    img: "bVsgyKr.jpg",
+    opts: {
+      type: ["EDs"]
+    }
+  }
+];
